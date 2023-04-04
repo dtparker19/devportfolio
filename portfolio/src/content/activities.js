ca752a3e30ab57dd882fc46d15761c4b254a24db
david parker <david@prodigytutors.com>
2023-04-04 17:40:07 +0000

    initial project commit

---
+++ portfolio/src/content/activities.js
@@ -0,0 +1,16 @@
+export const activities = [
+    {
+      title: "Blog Writing",
+      text: "Avid blogger on Medium, Blogger, and my personal blog",
+    },
+    {
+      title: "Webinar Speaker",
+      text: "Masterclass speaker for various events and topics",
+    },
+    {
+      title: "ReactConf Attendee",
+      text: "Attended conference on React",
+    },
+  ];
+  
+  
---
+++ portfolio/src/content/services.js
@@ -0,0 +1,18 @@
+export const services = [
+    {
+      icon: "/assets/services/color-wheel.png",
+      title: "Web design",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      icon: "/assets/services/photography.png",
+      title: "Photography",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      icon: "/assets/services/ui.png",
+      title: "UI/UX expert",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+  ];
+  
